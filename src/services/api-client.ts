41dd1a24db567965988ec18fca7b921a99f2ea4c
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2023-09-14 02:42:30 +0530

    Create custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://api.rawg.io/api',
-    params:{
-        key: 'a9a66bd0141a4cfcb2ceab5b642a5823'
-    }
-})
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: 'a9a66bd0141a4cfcb2ceab5b642a5823',
+  },
+});
